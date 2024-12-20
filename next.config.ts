88dbc84f43b518b8f8c789bb8af9d500732a60cd
SachinduNethmin <108050026+Sachindu-Nethmin@users.noreply.github.com>
2024-12-20 14:45:46 +0530

    Update next.config.ts
    fix video

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,20 @@ const nextConfig: NextConfig = {
     domains: [], // Add any external image domains if needed
   },
   assetPrefix: "/photography-web/",
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(mp4|webm)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: `${process.env.NEXT_PUBLIC_BASE_PATH || ''}/videos`,
+          outputPath: 'static/videos',
+          name: '[name].[ext]',
+        },
+      },
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
