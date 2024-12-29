520d4b3197e570577015fa81774324d02964190d
SachinduNethmin <108050026+Sachindu-Nethmin@users.noreply.github.com>
2024-12-29 22:44:49 +0530

    Update gallery.tsx

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -3,46 +3,46 @@ export default function Gallery(){
     <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
       <div className="grid gap-4">
         <div>
-          <img className="h-auto max-w-full rounded-lg" src="./images/r1.png" alt="Logo" />
+          <img className="h-auto w-auto rounded-lg" src="./images/r1.png" alt="Logo" />
         </div>
         <div>
-          <img className="h-auto max-w-full rounded-lg" src="./images/r1.png" alt="Logo" />
+          <img className="h-auto w-auto rounded-lg" src="./images/r1.png" alt="Logo" />
         </div>
         <div>
-          <img className="h-auto max-w-full rounded-lg" src="./images/r1.png" alt="Logo" />
-        </div>
-      </div>
-      <div className="grid gap-4">
-        <div>
-          <img className="h-auto max-w-full rounded-lg" src="./images/r1.png" alt="Logo" />
-        </div>
-        <div>
-          <img className="h-auto max-w-full rounded-lg" src="./images/r1.png" alt="Logo" />
-        </div>
-        <div>
-          <img className="h-auto max-w-full rounded-lg" src="./images/r1.png" alt="Logo" />
+          <img className="h-auto w-auto rounded-lg" src="./images/r1.png" alt="Logo" />
         </div>
       </div>
       <div className="grid gap-4">
         <div>
-          <img className="h-auto max-w-full rounded-lg" src="./images/r1.png" alt="Logo" />
+          <img className="h-auto w-auto rounded-lg" src="./images/r1.png" alt="Logo" />
         </div>
         <div>
-          <img className="h-auto max-w-full rounded-lg" src="./images/r1.png" alt="Logo" />
+          <img className="h-auto w-auto rounded-lg" src="./images/r1.png" alt="Logo" />
         </div>
         <div>
-          <img className="h-auto max-w-full rounded-lg" src="./images/r1.png" alt="Logo" />
+          <img className="h-auto w-auto rounded-lg" src="./images/r1.png" alt="Logo" />
         </div>
       </div>
       <div className="grid gap-4">
         <div>
-          <img className="h-auto max-w-full rounded-lg" src="./images/r1.png" alt="Logo" />
+          <img className="h-auto w-auto rounded-lg" src="./images/r1.png" alt="Logo" />
         </div>
         <div>
-          <img className="h-auto max-w-full rounded-lg" src="./images/r1.png" alt="Logo" />
+          <img className="h-auto w-auto rounded-lg" src="./images/r1.png" alt="Logo" />
         </div>
         <div>
-          <img className="h-auto max-w-full rounded-lg" src="./images/r1.png" alt="Logo" />
+          <img className="h-auto w-auto rounded-lg" src="./images/r1.png" alt="Logo" />
+        </div>
+      </div>
+      <div className="grid gap-4">
+        <div>
+          <img className="h-auto w-auto rounded-lg" src="./images/r1.png" alt="Logo" />
+        </div>
+        <div>
+          <img className="h-auto w-auto rounded-lg" src="./images/r1.png" alt="Logo" />
+        </div>
+        <div>
+          <img className="h-auto w-auto rounded-lg" src="./images/r1.png" alt="Logo" />
         </div>
       </div>
     </div>
