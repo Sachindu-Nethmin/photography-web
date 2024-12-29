ebdb2804984a70a92ceb3ac11af89b0321cdf981
SachinduNethmin <108050026+Sachindu-Nethmin@users.noreply.github.com>
2024-12-29 22:22:23 +0530

    Update gallery.tsx

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -1,3 +1,50 @@
-export default function gallery() {
-    return <div>Gallery</div>
-  }
+export default function Gallery(){
+  return (
+    <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+      <div className="grid gap-4">
+        <div>
+          <img className="h-auto max-w-full rounded-lg" src="./images/Logo.png" alt="Logo" />
+        </div>
+        <div>
+          <img className="h-auto max-w-full rounded-lg" src="./images/Logo.png" alt="Logo" />
+        </div>
+        <div>
+          <img className="h-auto max-w-full rounded-lg" src="./images/Logo.png" alt="Logo" />
+        </div>
+      </div>
+      <div className="grid gap-4">
+        <div>
+          <img className="h-auto max-w-full rounded-lg" src="./images/Logo.png" alt="Logo" />
+        </div>
+        <div>
+          <img className="h-auto max-w-full rounded-lg" src="./images/Logo.png" alt="Logo" />
+        </div>
+        <div>
+          <img className="h-auto max-w-full rounded-lg" src="./images/Logo.png" alt="Logo" />
+        </div>
+      </div>
+      <div className="grid gap-4">
+        <div>
+          <img className="h-auto max-w-full rounded-lg" src="./images/Logo.png" alt="Logo" />
+        </div>
+        <div>
+          <img className="h-auto max-w-full rounded-lg" src="./images/Logo.png" alt="Logo" />
+        </div>
+        <div>
+          <img className="h-auto max-w-full rounded-lg" src="./images/Logo.png" alt="Logo" />
+        </div>
+      </div>
+      <div className="grid gap-4">
+        <div>
+          <img className="h-auto max-w-full rounded-lg" src="./images/Logo.png" alt="Logo" />
+        </div>
+        <div>
+          <img className="h-auto max-w-full rounded-lg" src="./images/Logo.png" alt="Logo" />
+        </div>
+        <div>
+          <img className="h-auto max-w-full rounded-lg" src="./images/Logo.png" alt="Logo" />
+        </div>
+      </div>
+    </div>
+  );
+}
