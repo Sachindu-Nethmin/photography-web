be1137543df364409bdf95d3f6a4ff2e348400b8
SachinduNethmin <108050026+Sachindu-Nethmin@users.noreply.github.com>
2024-12-30 00:52:29 +0530

    Update gallery.tsx

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -1,9 +1,9 @@
 export default function gallery() {
-    return<div className="grid grid-cols-3 gap-4">
+    return(<div className="grid grid-cols-3 gap-4">
     <div className="bg-gray-200 p-4">Item 1</div>
     <div className="bg-gray-200 p-4">Item 2</div>
     <div className="bg-gray-200 p-4">Item 3</div>
     <div className="bg-gray-200 p-4">Item 4</div>
-  </div>
+  </div>)
   
   }
