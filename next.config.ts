c529b0f3fd3262549d3abf23bd9ed4d7b47d7be7
SachinduNethmin <108050026+Sachindu-Nethmin@users.noreply.github.com>
2025-01-27 16:19:58 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  distDir: "dist",
   images: {
     unoptimized: true,
     domains: [], // Add any external image domains if needed
